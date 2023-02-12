735b2a25d76ddc8e9acce0e51b5de609c330b3f7
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-02-12 17:01:55 +0500

    Create send.py

---
+++ 9-dars/send.py
@@ -0,0 +1,18 @@
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your-username@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+python manage.py shell
+>>> from django.core.mail import send_mail
+>>> send_mail('test email', 'hello world', to=['test@email.com'])
+
+
+python manage.py shell
+>>> from django.core.mail import send_mail
+>>> send_mail('test email', 'hello world', 'your@email.com', ['test@email.com'])
+
+
+
