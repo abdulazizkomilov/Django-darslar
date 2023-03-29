441bd58e83fc594dd2d40e670d0a1f73c9f34e62
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-03-29 15:08:29 +0500

    Update views.py

--- blog_test/blog/views.py
+++ blog_test/blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Articles
+from django.db.models import Q
+
+def search(request):
+    query = request.GET.get('query', '')
+    posts = Articles.objects.filter(Q(title__icontains=query) | Q(text__icontains=query))
+
+    context = {
+        'query': query,
+        'posts': posts,
+    }
+
+    return render(request, 'search.html', context)
 
 def front_page(request):
 
