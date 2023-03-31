d498ffbc8cd11633a6a6c45f1306acc35f770305
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-03-31 14:33:43 +0500

    Pagination
    Pagination

--- blog_test/blog/views.py
+++ blog_test/blog/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Articles
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def search(request):
     query = request.GET.get('query', '')
     posts = Articles.objects.filter(Q(title__icontains=query) | Q(text__icontains=query))
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 2)
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     context = {
         'query': query,
@@ -17,6 +27,15 @@ def search(request):
 def front_page(request):
 
     posts = Articles.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 2)
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     context = {
         'posts': posts,
