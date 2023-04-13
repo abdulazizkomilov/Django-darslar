a64efa7bc95feffa9d7a92ca4d5a07d4d2bc873a
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-13 12:50:47 +0500

    Create urls.py

---
+++ coffee/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<str:pk>/', views.blog_detail, name='blog_detail'),
+    path('delete-commnet/<str:pk>/<str:id>/', views.delete_comment, name='delete_comment'),
+]
