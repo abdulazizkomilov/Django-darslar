1b45d828b1ccc426346a856f0a44237554573e67
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-15 08:53:36 +0500

    Update urls.py

--- coffee/blog/urls.py
+++ coffee/blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', views.blog, name='blog'),
+    path('account/<str:pk>/', views.account, name='account'),
     path('blog/<str:pk>/', views.blog_detail, name='blog_detail'),
     path('delete-commnet/<str:pk>/<str:id>/', views.delete_comment, name='delete_comment'),
 ]
