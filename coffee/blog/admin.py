d796e1bc748e2b59c6a1226805dbe02f1a769d26
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-15 08:52:49 +0500

    Update admin.py

--- coffee/blog/admin.py
+++ coffee/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Blog, Comment
+from .models import Blog, Comment, Userprofile
 
 
 admin.site.register(Blog)
 admin.site.register(Comment)
+admin.site.register(Userprofile)
