af433aba3ea10984939b12e8a9b47910977cfd95
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-04-26 15:27:36 +0500

    Update views.py

--- coffee/blog/views.py
+++ coffee/blog/views.py
@@ -1,6 +1,85 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Blog, Comment
-from django.contrib.auth.models import User
+from .models import Blog, Comment, User
+
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages #flashmessages
+from .forms import MyUserCreationForm, UserForm
+from django.contrib.auth.decorators import login_required
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('blog:home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('blog:home')
+        else:
+            messages.error(request, 'Username OR password does not exit')
+
+    context = {
+        'page': page,
+    }
+    return render(request, 'registration/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('blog:home')
+
+
+def registerPage(request):
+    page = 'Sign Up'
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('blog:login')
+        else:
+            pass
+
+    else:
+        form = MyUserCreationForm()
+
+    context = {
+        'form': form,
+        'page': page,
+    }
+    return render(request, 'registration/login.html', context)
+
+
+
+@login_required(login_url='login')
+def updateUser(request, pk):
+    page = 'User Update'
+
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:account', pk=pk)
+        else:
+            messages.error(request, 'This username was already used')
+
+    return render(request, 'registration/update-user.html', {'form': form, 'page': page,})
+
 
 
 def home(request):
