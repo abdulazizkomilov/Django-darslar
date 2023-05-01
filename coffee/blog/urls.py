62b520036bd0ef646010ce70a7628ab66092c83c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-05-01 06:47:50 +0500

    Update urls.py

--- coffee/blog/urls.py
+++ coffee/blog/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('account/<str:pk>/', views.account, name='account'),
     path('account/delete-blog/<str:pk>/<str:id>', views.delete_blog, name='delete_blog'),
     path('blog/<str:pk>/', views.blog_detail, name='blog_detail'),
+    path('update-blog/<str:pk>/', views.blogUpdate, name='update_blog'),
+    path('like-blog/<str:pk>/', views.like_blog, name='like_blog'),
+    path('deslike-blog/<str:pk>/', views.deslike_blog, name='deslike_blog'),
+    path('follow-blog/<str:pk>/', views.follow_blog, name='follow_blog'),
+    path('unfollow-blog/<str:pk>/', views.unfollow_blog, name='unfollow_blog'),
     path('delete-commnet/<str:pk>/<str:id>/', views.delete_comment, name='delete_comment'),
 ]
