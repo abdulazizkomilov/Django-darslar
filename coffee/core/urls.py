54a854a9756c9a51804887c64ec28377802513d0
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-05-18 18:50:28 +0500

    Update urls.py

--- coffee/core/urls.py
+++ coffee/core/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('blog.urls', namespace='blog')),
 ]
 
